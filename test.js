a08899c6e3abdf23b7c36b0e82982358d2f6d2fa
davidlira1 <liradavid100@gmail.com>
2021-04-07 20:11:24 -0700

    implemented firebase utils, included ability to store authenticated users into firebase database

---
+++ test.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('gQtsjjPIBoRm8PhyfWRv').collection('cartItems').doc('6Hx4AKcCY8Zi9TzTtLQi');
